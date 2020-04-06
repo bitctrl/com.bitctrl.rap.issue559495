d63e0c28f8d5d8cd35e9f81e52725123a53c532a
Falko Schumann <falko.schumann@muspellheim.de>
2020-04-06 15:29:09 +0200

    Init project

---
+++ src/com/bitctrl/rap/issue559495/BasicApplication.java
@@ -0,0 +1,19 @@
+package com.bitctrl.rap.issue559495;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.client.WebClient;
+
+
+public class BasicApplication implements ApplicationConfiguration {
+
+    public void configure(Application application) {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put(WebClient.PAGE_TITLE, "RAP Issue 559495");
+        application.addEntryPoint("/hello", BasicEntryPoint.class, properties);
+    }
+
+}
